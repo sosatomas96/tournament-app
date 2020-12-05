9110075392fc3db53e514457847ecc925a1a4d91
Manuel Francisco <franciscomanueljose@gmail.com>
2020-12-05 20:13:13 -0300

    Defined some guidelines for competitions

---
+++ scripts/my-competitions.js
@@ -0,0 +1,29 @@
+// let newCompetition = {
+//     id: '',
+//     name: '',
+//     startDate: new Date(),
+//     type: {
+//         name: 'league', //league, tournament
+//         getBattleDates: function () {
+//             return [
+//                 {
+//                     //date 1
+//                     name: 'date1',
+//                     order: 1,
+//                     startDate: new Date(),
+//                     battles: [
+//                         { player1: '', player2: '' },
+//                         { player1: '', player2: '' },
+//                         { player1: '', player2: '' },
+//                     ],
+//                 },
+//                 {},
+//             ];
+//         },
+//     },
+//     players: [{}, {}, {}],
+//     //...
+// };
+
+// function createCompetition(newCompetition) {
+// }
